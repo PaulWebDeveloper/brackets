f2e6539265c219165f4b34012c72393879e0b393
Paul <paulworkweb@gmail.com>
2020-03-03 19:31:34 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let n = 0, len = bracketsConfig.length; n <= len; n++) {
+    bracketsConfig.filter(el => {
+      let pattern = el.join('')
+      for (let j = 0, lenstr = str.length / 2; j < lenstr; j++)
+        str = str.replace(pattern, '');
+    });
+  }
+  return !str;
 }
